4bed10d9953f805d3073ccc359f3bb0144cbda21
1juyoung <myjuyoung2@gmail.com>
2025-06-20 23:31:27 +0900

    feat: kakao login 구현 #8

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+const authOptions = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, email }) {
+      return true;
+    },
+
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+});
+
+export { authOptions as GET, authOptions as POST };
